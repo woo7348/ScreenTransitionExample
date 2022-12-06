4313d0e1f1f410387f894ef16082b38b8a268f25
MinWoo <mynooo7688@gmail.com>
2022-12-07 00:07:26 +0900

    ScreenTransition Practice

--- ScreenTransitionExample/ViewController.swift
+++ ScreenTransitionExample/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func tapCodePushButton(_ sender: UIButton) {
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") else { return }
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    @IBAction func tapCodePresent(_ sender: UIButton) {
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePresentViewController") else { return }
+        self.present(viewController, animated: true, completion: nil)
+    }
 }
-
